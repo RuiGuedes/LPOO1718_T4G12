0ee7c7abcd2c5d3224a103ad2b361af56b52415d
Rui <Rui@DESKTOP-Q4SJA80>
2018-02-17 19:20:18 +0000

    Task5 completa

--- LPOO1718_T4G12/src/BasicGameLogic.java
+++ LPOO1718_T4G12/src/BasicGameLogic.java
@@ -1,15 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.Random;
 
 
 public class BasicGameLogic {
 
 	public static void main(String[] args) {
 
-		if(firstDungeon() == 0)
-			secondDungeon();
-	
+		//if(firstDungeon() == 0)
+		secondDungeon();
+
 	}
 
 	public static int firstDungeon() {
@@ -35,7 +36,7 @@ public class BasicGameLogic {
 		int xH = 1;
 		int yH = 1;
 
-		//Hero position
+		//Guard position
 		int xG = 1;
 		int yG = 8;
 
@@ -114,17 +115,165 @@ public class BasicGameLogic {
 				return 1;
 			}	
 		}
-		
+
 		return 0;
 	}
-	
+
 	public static int secondDungeon() {
-		
-		
+
+		//Game map
+		char[][] map = { 
+				{'X','X','X','X','X','X','X','X','X'},
+				{'I',' ',' ',' ','O',' ',' ','k','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'}, 
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'}, 
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'}, 
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'}, 
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X','H',' ',' ',' ',' ',' ',' ','X'}, 
+				{'X','X','X','X','X','X','X','X','X'} };
+
+		//Hero position
+		int xH = 7;
+		int yH = 1;
+
+		//Ogre position
+		int xO = 1;
+		int yO = 4;
+
+		//Variables
+		char input = ' ';
+		boolean keyStatus = false; 
+
+		while(input != 'e')
+		{	
+			printGameMap(map);
+
+			input = readInput();
+
+			//Hero Movement
+			switch(input)
+			{
+			case 'w':
+				if((map[xH-1][yH] == ' ') || (map[xH-1][yH] == 'k') || (map[xH-1][yH] == 'K')){
+					map[xH][yH] = ' ';
+					xH--;
+					map[xH][yH] = 'H';
+				}
+				break;
+			case 's':
+				if(map[xH+1][yH] == ' '){
+					if((xH == 1) && (yH == 7))
+					{
+						map[xH][yH] = 'K';
+						keyStatus = true;
+					}
+					else
+						map[xH][yH] = ' ';
+					xH++;
+					map[xH][yH] = 'H';
+				}
+				break;
+			case 'a':
+				if(map[xH][yH-1] == ' '){
+					if((xH == 1) && (yH == 7))
+					{
+						map[xH][yH] = 'K';
+						keyStatus = true;
+					}
+					else
+						map[xH][yH] = ' ';
+					yH--;
+					map[xH][yH] = 'H';
+				}
+				else if(map[xH][yH-1] == 'I')
+				{
+					if(keyStatus)
+						map[xH][yH-1] = 'S';
+				}
+				else if(map[xH][yH-1] == 'S')
+				{
+					System.out.println("\n  VICTORY !!!");
+					input = 'e';
+				}
+				break;
+			case 'd':
+				if((map[xH][yH+1] == ' ') || (map[xH][yH+1] == 'K') || (map[xH][yH+1] == 'k')){
+					map[xH][yH] = ' ';
+					yH++;
+					map[xH][yH] = 'H';
+				}
+				break;
+			default:
+				break;
+			}
+
+			//Ogre Movement
+			Random rand = new Random();
+			int move = rand.nextInt(4) + 1;
+
+			switch(move)
+			{
+			case 1:	//Moves up
+				if(map[xO-1][yO] != 'X') {	
+					map[xO][yO] = ' ';
+					xO--;
+					if((xO == 1) && (yO == 7))
+						map[xO][yO] = '$';
+					else
+						map[xO][yO] = 'O';
+				}
+				break;
+			case 2:	//Moves down
+				if(map[xO+1][yO] != 'X') {	
+					if((xO == 1) && (yO == 7) && keyStatus)
+						map[xO][yO] = 'K';
+					else if((xO == 1) && (yO == 7))
+						map[xO][yO] = 'k';
+					else
+						map[xO][yO] = ' ';
+
+					xO++;
+					map[xO][yO] = 'O';		
+				}
+				break;
+			case 3:	//Moves left
+				if((map[xO][yO-1] != 'X') && (map[xO][yO-1] != 'I') && (map[xO][yO-1] != 'S'))  {	
+					if((xO == 1) && (yO == 7) && keyStatus)
+						map[xO][yO] = 'K';
+					else if((xO == 1) && (yO == 7))
+						map[xO][yO] = 'k';
+					else
+						map[xO][yO] = ' ';
+
+					yO--;
+					map[xO][yO] = 'O';		
+				}
+				break;
+			case 4:	//Moves right
+				if(map[xO][yO+1] != 'X') {
+					map[xO][yO] = ' ';
+					yO++;
+					if((xO == 1) && (yO == 7))
+						map[xO][yO] = '$';
+					else
+						map[xO][yO] = 'O';
+				}
+				break;
+			}
+
+			if((map[xH+1][yH] == 'O') || (map[xH-1][yH] == 'O') || (map[xH][yH+1] == 'O') || (map[xH][yH-1] == 'O'))
+			{
+				printGameMap(map);
+				System.out.println("\n  GAME OVER :(");
+				return 1;
+			}	
+		}
+
 		return 0;
 	}
-	
-	
+
+
 	public static char readInput() {
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));

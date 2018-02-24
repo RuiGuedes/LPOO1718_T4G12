4051957f1591b323e60a955c8d569d39032b9f82
Rui <Rui@DESKTOP-Q4SJA80>
2018-02-24 21:29:28 +0000

    Guided Project 2 - Task3 quase completa

--- LPOO1718_T4G12/src/dkeep/logic/Ogre.java
+++ LPOO1718_T4G12/src/dkeep/logic/Ogre.java
@@ -10,62 +10,84 @@ public class Ogre extends Elements {
 	public int clubX = 2;
 	public int clubY = 4;
 
+	public int stop = 0;
+
 	public Ogre() {
 		this.x = 1;
 		this.y = 4;
 	}
 
-	public void ogreMovement(char[][] tmpMap) {
+	public void ogreMovement(int heroX, int heroY, char[][] tmpMap) {
 
-		//Variables
-		Random rand = new Random();
-		int move;
-
-		//Generates club new position
-		do	{
-			move = rand.nextInt(4) + 1;
-		}while(((move == 1) && (x == 1)) || ((move == 2) && (x == 7)) || ((move == 3) && (y == 1)) || ((move == 4) && (y == 7)));
-
-		//Ogre Movement
-		switch(move)
+		if( ((x == heroX) && (y == (heroY + 1))) || ((x == heroX) && (y == (heroY - 1))) || 
+				((y == heroY) && (x == (heroX + 1))) || ((y == heroY) && (x == (heroX - 1))))
 		{
-		case 1:	//Moves up
-			if(tmpMap[x-1][y] != 'X') {	
-				x--;
-				if((x == 1) && (y == 7))
-					state = '$';
-				else
-					state = 'O';
+			if(state != '8') {
+				state = '8';
+				stop = 2;
 			}
-			break;
-		case 2:	//Moves down
-			if(tmpMap[x+1][y] != 'X') {	
-				if((x == 1) && (y == 7))
-					state = 'O';
-
-				x++;
-			}
-			break;
-		case 3:	//Moves left
-			if((tmpMap[x][y-1] != 'X') && (tmpMap[x][y-1] != 'I') && (tmpMap[x][y-1] != 'S'))  {	
-				if((x == 1) && (y == 7))
-					state = 'O';
-
-				y--;		
-			}
-			break;
-		case 4:	//Moves right
-			if(tmpMap[x][y+1] != 'X') {
-				y++;
-				if((x == 1) && (y == 7))
-					state = '$';
-				else
-					state = 'O';
-			}
-			break;
 		}
+		else
+		{	
+			if(stop != 0) {
+				stop--;
+				if(stop == 0)
+					state = 'O';
+			}
+			else
+			{
+				//Variables
+				Random rand = new Random();
+				int move;
 
-		clubMovement(tmpMap);
+				//Generates club new position
+				do	{
+					move = rand.nextInt(4) + 1;
+				}while(((move == 1) && (x == 1)) || ((move == 2) && (x == 7)) || ((move == 3) && (y == 1)) || ((move == 4) && (y == 7)));
+
+				//Ogre Movement
+				switch(move)
+				{
+				case 1:	//Moves up
+					if(tmpMap[x-1][y] != 'X') {	
+						x--;
+						if((x == 1) && (y == 7))
+							state = '$';
+						else
+							state = 'O';
+					}
+					break;
+				case 2:	//Moves down
+					if(tmpMap[x+1][y] != 'X') {	
+						if((x == 1) && (y == 7))
+							state = 'O';
+
+						x++;
+					}
+					break;
+				case 3:	//Moves left
+					if((tmpMap[x][y-1] != 'X') && (tmpMap[x][y-1] != 'I') && (tmpMap[x][y-1] != 'S'))  {	
+						if((x == 1) && (y == 7))
+							state = 'O';
+
+						y--;		
+					}
+					break;
+				case 4:	//Moves right
+					if(tmpMap[x][y+1] != 'X') {
+						y++;
+						if((x == 1) && (y == 7))
+							state = '$';
+						else
+							state = 'O';
+					}
+					break;
+				}
+			}
+		}
+		
+		if(stop != 2)
+			clubMovement(tmpMap);
 	}
 
 	public void clubMovement(char[][] tmpMap) {
@@ -79,7 +101,7 @@ public class Ogre extends Elements {
 			club = rand.nextInt(4) + 1;
 		}while(((club == 1) && (x == 1)) || ((club == 2) && (x == 7)) || ((club == 3) && (y == 1)) || ((club == 4) && (y == 7)));
 
-		
+
 		//Club new position
 		switch(club)
 		{
@@ -96,7 +118,7 @@ public class Ogre extends Elements {
 			if(tmpMap[x+1][y] == 'H') {
 				Game.gameState = Game.GameState.GAMEOVER;
 			}
-			
+
 			clubX = x + 1;
 			clubY = y;
 			break;

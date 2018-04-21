586261affca11e9950f05508de3bf307db584054
Rui Guedes <ruiguedes12@hotmail.com>
2018-04-21 11:20:29 +0100

    Tentativa de organização do repositório

---
+++ Unstoppable_Bros/desktop/src/com/ubros/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ubros.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ubros.game.UbrosGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UbrosGame(), config);
+	}
+}

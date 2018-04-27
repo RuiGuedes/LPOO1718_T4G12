783606e6cc1800ac538cc4cfd41cb74524688ab4
Rui Guedes <ruiguedes12@hotmail.com>
2018-04-27 17:46:31 +0100

    Verificacao do IP

---
+++ Unstoppable_Bros/core/src/com/ubros/game/Networking/Connection.java
@@ -0,0 +1,46 @@
+package com.ubros.game.Networking;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+public class Connection {
+
+    public Connection() {
+
+        initializeServer();
+    }
+
+    private void initializeServer() {
+
+        List<String> addresses = new ArrayList<String>();
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            for(NetworkInterface ni : Collections.list(interfaces)){
+                for(InetAddress address : Collections.list(ni.getInetAddresses()))
+                {
+                    if(address instanceof Inet4Address){
+                        addresses.add(address.getHostAddress());
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        // Print the contents of our array to a string.  Yeah, should have used StringBuilder
+        String ipAddress = new String("");
+        for(String str:addresses)
+        {
+            ipAddress = ipAddress + str + "\n";
+        }
+
+        System.out.print("FODEU !!!");
+        System.out.print(ipAddress);
+    }
+}

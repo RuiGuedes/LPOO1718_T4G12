617038c2802ce2eac125aa409eaa1f35f50e17a7
Rui Guedes <ruiguedes12@hotmail.com>
2018-05-22 23:56:31 +0100

    Desenvolvimento do código relativo ao movimento do jogador

--- Unstoppable_Bros/core/src/com/ubros/game/Gui/PlayGameScreen.java
+++ Unstoppable_Bros/core/src/com/ubros/game/Gui/PlayGameScreen.java
@@ -15,6 +15,9 @@ import com.ubros.game.UbrosGame;
 import com.ubros.game.View.Elements.ElementView;
 import com.ubros.game.View.Elements.HeroView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
 
     /**
@@ -31,6 +34,11 @@ public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
      *
      */
     public static final float PIXEL_TO_METER = 100;
+
+    private static final float BUTTON_WIDTH = (int) (SCREEN_WIDTH * 0.08) / PIXEL_TO_METER;
+
+    private static final float BUTTON_HEIGHT = (int) (SCREEN_HEIGHT * 0.08) / PIXEL_TO_METER;
+
 
     /**
      * The game this screen belongs to.
@@ -52,6 +60,13 @@ public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
      */
     private OrthogonalTiledMapRenderer mapRenderer;
 
+
+    private Texture leftButton;
+    private Texture rightButton;
+    private Texture bulletButton;
+    private Texture jumpButton;
+
+    private List<Texture> buttonTextures = new ArrayList<Texture>();
 
     ////////////////////////////
 
@@ -75,7 +90,7 @@ public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
 
         this.mapLoader = new TmxMapLoader();
         UbrosGame.map = this.mapLoader.load("UbrosMap/UbrosMap.tmx");
-        this.mapRenderer = new OrthogonalTiledMapRenderer(UbrosGame.map, 1/PIXEL_TO_METER);
+        this.mapRenderer = new OrthogonalTiledMapRenderer(UbrosGame.map, 1 / PIXEL_TO_METER);
 
 
         Gdx.input.setInputProcessor(this);
@@ -89,7 +104,7 @@ public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
 
     public void createCamera() {
 
-        this.gameCam = new OrthographicCamera(SCREEN_WIDTH / PIXEL_TO_METER, SCREEN_HEIGHT/PIXEL_TO_METER);
+        this.gameCam = new OrthographicCamera(SCREEN_WIDTH / PIXEL_TO_METER, SCREEN_HEIGHT / PIXEL_TO_METER);
 
         this.gameCam.position.set(gameCam.viewportWidth / 2f, gameCam.viewportHeight / 2f, 0);
         this.gameCam.update();
@@ -100,46 +115,37 @@ public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
      */
     private void loadAssets() {
         this.game.getAssetManager().load("background.jpg", Texture.class);
+        this.game.getAssetManager().load("moveLeftButtonOff.png", Texture.class);
+        this.game.getAssetManager().load("moveLeftButtonOn.png", Texture.class);
+        this.game.getAssetManager().load("moveRightButtonOff.png", Texture.class);
+        this.game.getAssetManager().load("moveRightButtonOn.png", Texture.class);
+        this.game.getAssetManager().load("jumpButtonOff.png", Texture.class);
+        this.game.getAssetManager().load("jumpButtonOn.png", Texture.class);
         this.game.getAssetManager().load("bulletButtonOff.png", Texture.class);
+        this.game.getAssetManager().load("bulletButtonOn.png", Texture.class);
         this.game.getAssetManager().load("Robot/Robot.pack", TextureAtlas.class);
 
         this.game.getAssetManager().finishLoading();
-        atlas = this.game.getAssetManager().get("Robot/Robot.pack");
-        hero = new HeroView(this.game, atlas);
-
+        initializeGraphics();
     }
 
-    public void handleInput(float delta) {
+    public void initializeGraphics() {
 
-        for(int i = 0; i < 3; i++) {
-            if (Gdx.input.isTouched(i)) {
+        this.atlas = this.game.getAssetManager().get("Robot/Robot.pack");
+        this.hero = new HeroView(this.game, atlas);
 
-                int x = Gdx.input.getX(i);
-                int y = Gdx.input.getY(i);
+        buttonTextures.add(game.getAssetManager().get("moveLeftButtonOff.png", Texture.class));
+        buttonTextures.add(game.getAssetManager().get("moveLeftButtonOn.png", Texture.class));
+        buttonTextures.add(game.getAssetManager().get("moveRightButtonOff.png", Texture.class));
+        buttonTextures.add(game.getAssetManager().get("moveRightButtonOn.png", Texture.class));
+        buttonTextures.add(game.getAssetManager().get("jumpButtonOff.png", Texture.class));
+        buttonTextures.add(game.getAssetManager().get("jumpButtonOn.png", Texture.class));
+        buttonTextures.add(game.getAssetManager().get("bulletButtonOff.png", Texture.class));
+        buttonTextures.add(game.getAssetManager().get("bulletButtonOn.png", Texture.class));
 
-                if ((x > SCREEN_WIDTH / 2) && (y > SCREEN_HEIGHT / 2) && (GameController.getInstance(this.game).getHero().body.getLinearVelocity().x <= 3))
-                    GameController.getInstance(this.game).getHero().body.applyLinearImpulse(new Vector2(0.3f, 0), GameController.getInstance(this.game).getHero().body.getWorldCenter(), true);
-
-                if ((x < (SCREEN_WIDTH / 2)) && (y > SCREEN_HEIGHT / 2) && (GameController.getInstance(this.game).getHero().body.getLinearVelocity().x >= -3))
-                    GameController.getInstance(this.game).getHero().body.applyLinearImpulse(new Vector2(-0.3f, 0), GameController.getInstance(this.game).getHero().body.getWorldCenter(), true);
-
-            }
-        }
+        resetButtons();
     }
 
-
-    public void update(float delta) {
-
-        handleInput(delta);
-
-        GameController.getInstance(this.game).getWorld().step(1/60f, 6, 2);
-        GameController.getInstance(this.game).update(delta);
-
-        game.getBatch().setProjectionMatrix(gameCam.combined);
-        gameCam.update();
-        this.mapRenderer.setView(gameCam);
-
-    }
 
     /**
      * Renders this screen.
@@ -151,8 +157,8 @@ public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
 
         this.update(delta);
 
-        Gdx.gl.glClearColor(1,0,0,1);
-        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         this.mapRenderer.render();
         GameController.getInstance(this.game).getDebugRenderer().render(GameController.getInstance(this.game).getWorld(), gameCam.combined);
@@ -164,19 +170,93 @@ public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
 
     }
 
+    public void update(float delta) {
+
+        handleInput();
+
+        GameController.getInstance(this.game).getWorld().step(1 / 60f, 6, 2);
+        GameController.getInstance(this.game).update(delta);
+
+        game.getBatch().setProjectionMatrix(gameCam.combined);
+        gameCam.update();
+        this.mapRenderer.setView(gameCam);
+
+    }
+
+    public void handleInput() {
+
+        for (int i = 0; i < 4; i++) {
+            if (Gdx.input.isTouched(i)) {
+                int x = Gdx.input.getX(i);
+                int y = Gdx.input.getY(i);
+
+                if(checkLeftButton(x, y)) {
+                    leftButton = buttonTextures.get(1);
+                    if (GameController.getInstance(this.game).getHero().body.getLinearVelocity().x >= -3)
+                        GameController.getInstance(this.game).getHero().body.applyLinearImpulse(new Vector2(-0.3f, 0), GameController.getInstance(this.game).getHero().body.getWorldCenter(), true);
+                }
+
+                if(checkRightButton(x, y)) {
+                    rightButton = buttonTextures.get(3);
+                    if ((GameController.getInstance(this.game).getHero().body.getLinearVelocity().x <= 3))
+                        GameController.getInstance(this.game).getHero().body.applyLinearImpulse(new Vector2(0.3f, 0), GameController.getInstance(this.game).getHero().body.getWorldCenter(), true);
+                }
+            }
+        }
+    }
+
     public void drawElements(float delta) {
         hero.update(delta, GameController.getInstance(this.game).getHero());
         hero.draw(this.game.getBatch());
 
     }
 
-
     public void drawInteractiveButtons() {
-        System.out.print("RUI\n");
-        Texture bullet = game.getAssetManager().get("bulletButtonOff.png", Texture.class);
-        game.getBatch().draw(bullet, (SCREEN_WIDTH - 176)/PIXEL_TO_METER, 47/PIXEL_TO_METER, 112/PIXEL_TO_METER, 75/PIXEL_TO_METER);
-        //Texture background = game.getAssetManager().get("background.jpg", Texture.class);
-       // game.getBatch().draw(background, 0, 0,SCREEN_WIDTH/PIXEL_TO_METER,SCREEN_HEIGHT/PIXEL_TO_METER);
+        game.getBatch().draw(leftButton, (int) (SCREEN_WIDTH * 0.035) / PIXEL_TO_METER, (int) (SCREEN_HEIGHT * 0.04) / PIXEL_TO_METER, BUTTON_WIDTH, BUTTON_HEIGHT);
+        game.getBatch().draw(rightButton, (int) (SCREEN_WIDTH * 0.035 + BUTTON_WIDTH * PIXEL_TO_METER + SCREEN_WIDTH * 0.01) / PIXEL_TO_METER, (int) (SCREEN_HEIGHT * 0.04) / PIXEL_TO_METER, BUTTON_WIDTH, BUTTON_HEIGHT);
+        game.getBatch().draw(jumpButton, (int) (SCREEN_WIDTH * 0.89) / PIXEL_TO_METER, (int) (SCREEN_HEIGHT * 0.04 + BUTTON_HEIGHT * PIXEL_TO_METER + SCREEN_HEIGHT * 0.01) / PIXEL_TO_METER, BUTTON_WIDTH, BUTTON_HEIGHT);
+        game.getBatch().draw(bulletButton, (int) (SCREEN_WIDTH * 0.89) / PIXEL_TO_METER, (int) (SCREEN_HEIGHT * 0.04) / PIXEL_TO_METER, BUTTON_WIDTH, BUTTON_HEIGHT);
+    }
+
+    public void resetButtons() {
+        this.leftButton = buttonTextures.get(0);
+        this.rightButton = buttonTextures.get(2);
+        this.jumpButton = buttonTextures.get(4);
+        this.bulletButton = buttonTextures.get(6);
+    }
+
+    public boolean checkLeftButton(int x, int y) {
+        if ((x >= (int) (SCREEN_WIDTH * 0.035)) && (x <= (int) (SCREEN_WIDTH * 0.035 + BUTTON_WIDTH * PIXEL_TO_METER) &&
+                (y <= ((int) (SCREEN_HEIGHT * 0.96))) && (y >= (int) (SCREEN_HEIGHT * 0.96 - BUTTON_HEIGHT * PIXEL_TO_METER))))
+           return true;
+
+        return false;
+    }
+
+    public boolean checkRightButton(int x, int y) {
+        if ((x >= (int) (SCREEN_WIDTH * 0.035 + BUTTON_WIDTH * PIXEL_TO_METER + SCREEN_WIDTH * 0.01)) && (x <= (int) (SCREEN_WIDTH * 0.035 + BUTTON_WIDTH * PIXEL_TO_METER + SCREEN_WIDTH * 0.01 + BUTTON_WIDTH * PIXEL_TO_METER) &&
+                (y <= ((int) (SCREEN_HEIGHT * 0.96))) && (y >= (int) (SCREEN_HEIGHT * 0.96 - BUTTON_HEIGHT * PIXEL_TO_METER))))
+            return true;
+
+        return false;
+    }
+
+    public boolean checkJumpButton(int x, int y) {
+        if ((x <= ((int) (SCREEN_WIDTH * 0.89) + BUTTON_WIDTH * PIXEL_TO_METER)) && (x >= (int) (SCREEN_WIDTH * 0.89)) &&
+                (y >= ((int) (SCREEN_HEIGHT - (SCREEN_HEIGHT * 0.04 + BUTTON_HEIGHT * PIXEL_TO_METER + SCREEN_HEIGHT * 0.01) - BUTTON_HEIGHT * PIXEL_TO_METER))) &&
+                (y <= ((int) (SCREEN_HEIGHT - (SCREEN_HEIGHT * 0.04 + BUTTON_HEIGHT * PIXEL_TO_METER + SCREEN_HEIGHT * 0.01)))))
+            return true;
+
+        return false;
+    }
+
+    public boolean checkBulletButton(int x, int y) {
+        if ((x <= ((int) (SCREEN_WIDTH * 0.89) + BUTTON_WIDTH * PIXEL_TO_METER)) && (x >= (int) (SCREEN_WIDTH * 0.89)) &&
+                (y >= ((int) (SCREEN_HEIGHT - (SCREEN_HEIGHT * 0.04 + BUTTON_HEIGHT * PIXEL_TO_METER)))) &&
+                (y <= ((int) (SCREEN_HEIGHT - SCREEN_HEIGHT * 0.04))))
+            return true;
+
+        return false;
     }
 
     @Override
@@ -196,16 +276,32 @@ public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-
-        if (screenY < SCREEN_HEIGHT / 2)
+        if (checkJumpButton(screenX, screenY)) {
+            jumpButton = buttonTextures.get(5);
+            //if (GameController.getInstance(this.game).getHero().body.getLinearVelocity().y == 0)  -> TO AVOID DOUBLE JUMP
             GameController.getInstance(this.game).getHero().body.applyLinearImpulse(new Vector2(0, 4f), GameController.getInstance(this.game).getHero().body.getWorldCenter(), true);
+        }
+
+        if (checkBulletButton(screenX, screenY))
+            bulletButton = buttonTextures.get(7);
 
         return true;
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        System.out.println("Up");
+        if(checkLeftButton(screenX,screenY))
+            leftButton = buttonTextures.get(0);
+
+        if(checkRightButton(screenX,screenY))
+            rightButton = buttonTextures.get(2);
+
+        if (checkJumpButton(screenX, screenY))
+            jumpButton = buttonTextures.get(4);
+
+        if (checkBulletButton(screenX, screenY))
+            bulletButton = buttonTextures.get(6);
+
         return true;
     }
 
@@ -225,23 +321,23 @@ public class PlayGameScreen extends ScreenAdapter implements InputProcessor {
     }
 
     @Override
-    public void show () {
+    public void show() {
     }
 
     @Override
-    public void hide () {
+    public void hide() {
     }
 
     @Override
-    public void pause () {
+    public void pause() {
     }
 
     @Override
-    public void resume () {
+    public void resume() {
     }
 
     @Override
-    public void dispose () {
+    public void dispose() {
         UbrosGame.map.dispose();
         this.mapRenderer.dispose();
         GameController.getInstance(this.game).getWorld().dispose();
